822beb8eb666b7c46f74faa01fa68be760a13850
OverLoad <kotofeykin2000@bk.ru>
2024-03-27 23:23:30 +0300

    Feat: Problem 1 solved

---
+++ Problem 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+ 
+class Program
+{
+    static void Main(string[] args)
+    {
+        string word = Console.ReadLine();
+        string result = "";
+        string withoutFirst = word.Substring(1);
+ 
+        if (word.All(char.IsUpper))
+        {
+            foreach (char c in word)
+            {
+                result += char.ToLower(c);
+            }
+ 
+            Console.WriteLine(result);
+        }
+        else if (withoutFirst.All(char.IsUpper))
+        {
+            result += char.ToUpper(word[0]);
+ 
+            foreach (char c in withoutFirst)
+            {
+                result += char.ToLower(c);
+            }
+ 
+            Console.WriteLine(result);
+        }
+        else
+        {
+            Console.Write(word);
+        }
+    }
+}
